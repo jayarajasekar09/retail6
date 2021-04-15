b7d731e6dea96d914095ec55cf95465a35ff6ba9
Jayarajasekar <ejayarajasekar09@gmail.com>
2021-04-15 22:14:12 +0530

    save

--- src/spark write/spark write.py
+++ src/spark write/spark write.py
@@ -1,6 +1,6 @@
 from pyspark.sql import SparkSession
 
-spark = SparkSession.builder.master('local').appName('Spark Write csv app').getOrCreate()
+spark = SparkSession.builder.master('local').appName('Spark save csv app').getOrCreate()
 
 orders = spark.read.format('csv').schema('order_id int, order_date date, customer_id int, order_status String') \
     .load('/Users/jayarajasekar/Documents/data-master/retail_db/orders/part-00000')
